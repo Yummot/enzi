4132a0309d68a117a12d9e700a0b1f06498113e3
Mot Yu <mot.yu@outlook.com>
2019-09-23 13:25:31 -0400

    add Dependency class for resolve dep

--- enzi/parse_config.py
+++ enzi/parse_config.py
@@ -4,8 +4,36 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class Dependency(object):
+    def __init__(self, name, info):
+        if 'path' in info and 'url' in info:
+            raise RuntimeError(
+                'Dependency {} cannot have path and url in the same time'.format(name))
+        
+        if 'path' in info and not 'url' in info:
+            self.is_remote = False
+            self.path = str(info['path'])
+        elif not 'path' in info and 'url' in info:
+            self.is_remote = True
+            self.url = str(info['url'])
+
+        if 'version' in info and 'commit' in info:
+            logger.warning(
+                'Dependency {} configuration has commit and version at the same time, only use version key'.format(name))
+        if 'version' in info and not 'commit' in info:
+            self.use_version = True
+            self.version = str(info['version'])
+        elif not 'version' in info and 'commit' in info:
+            self.use_version = False
+            self.commit = str(info['commit'])
+        else:
+            raise RuntimeError('Dependency {} has not version or commit key.'.format(name))
+        
+        self.name = name
+        # print(self.name, self.is_remote, self.version or self.commit, self.path or self.url)
+
 class Config(object):
-    def __init__(self, config_file, extract_dep_only = False):
+    def __init__(self, config_file, extract_dep_only=False):
         conf = conf = toml.load(config_file)
 
         if not conf:
@@ -13,6 +41,7 @@ class Config(object):
             raise RuntimeError('Config toml file is empty.')
 
         self.package = {}
+        self.dependencies = {}
         self.filesets = {}
         self.targets = {}
         self.tools = {}
@@ -32,9 +61,13 @@ class Config(object):
             fileset = {}
             if 'files' in v:
                 fileset['files'] = v['files']
-            if 'dependencies' in v:
-                fileset['dependencies'] = v['dependencies']
+            # if 'dependencies' in v:
+            #     fileset['dependencies'] = v['dependencies']
             self.filesets[k] = fileset
+
+        if 'dependencies' in conf:
+            for dep, dep_conf in conf['dependencies'].items():
+                self.dependencies[dep] = Dependency(dep, dep_conf)
 
         if not extract_dep_only:
             # targets configs
