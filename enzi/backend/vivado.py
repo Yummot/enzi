e7995d44d8ded26ca5b79be7dbbba90a134bfbe3
Mot Yu <mot.yu@outlook.com>
2019-10-20 10:52:50 -0400

    improve vivado scripts

--- enzi/backend/vivado.py
+++ enzi/backend/vivado.py
@@ -21,11 +21,13 @@ class Vivado(Backend):
     # TODO: add Windows support
     supported_system = ('Linux', )
 
+    __work_dir__ = 'vivado-synth'
+
     @staticmethod
     def get_version():
         try:
             output = subprocess.check_output(['vivado', '-version'],  # pylint: disable=E1123
-                                         stdin=subprocess.PIPE)
+                                             stdin=subprocess.PIPE)
             return output.decode('utf-8').splitlines()[0]
         except Exception as e:
             logger.error(e)
@@ -33,22 +35,85 @@ class Vivado(Backend):
 
     def __init__(self, config={}, work_root=None):
         self.version = Vivado.get_version()
-        
+
+        # vivado work root
+        if not work_root:
+            work_root = Vivado.__work_dir__
+        else:
+            work_root = os.path.join(work_root, Vivado.__work_dir__)
+
         super(Vivado, self).__init__(config=config, work_root=work_root)
-        
+
         self.bitstream_name = config.get('bitstream_name', self.name)
-        
+
         self.device_part = config.get('device_part')
         if not self.device_part:
             logger.error('No device_part is provided.')
             raise SystemExit(1)
-        
+
         self.vlog_params = config.get('vlog_params', {})
         self.generics = config.get('generics', {})
         self.vlog_defines = config.get('vlog_defines', {})
         self.src_files = config.get('src_files', [])
         self.inc_dirs = config.get('inc_dirs', [])
-        
+
         has_xci = any(filter(lambda x: 'xci' in x, self.src_files))
         self.has_xci = has_xci
-        
+
+        # for vivado gen scripts' name only available after rendering the scripts
+        self._gen_scripts_name = None
+
+    @property
+    def _makefile_vars(self):
+        return {'name': self.name}
+
+    @property
+    def _program_vars(self):
+        return {
+            'name': self.name,
+            'device_part': self.device_part
+        }
+
+    @property
+    def _project_vars(self):
+        return {
+            'name': self.name,
+            'vlog_params': self.vlog_params,
+            'generics': self.generics,
+            'vlog_defines': self.vlog_defines,
+            'src_files': self.src_files,
+            'inc_dirs': self.inc_dirs,
+            'toplevel': self.toplevel,
+            'has_xci': self.has_xci
+        }
+    
+
+    def gen_scripts(self):
+        name = self.name
+        mk = 'Makefile'
+        proj_tcl = name + '.tcl'
+        prog_tcl = name + '_pgm.tcl'
+        run_tcl = name + '_run.tcl'
+        synth_tcl = name + '_synth.tcl'
+
+        self.render_template('vivado_makefile.j2', mk, self._makefile_vars)
+        self.render_template('vivado_project.tcl.j2', proj_tcl, self._project_vars)
+        self.render_template('vivado_program.tcl.j2', prog_tcl, self._program_vars)
+        self.render_template('vivado_run.tcl.j2', run_tcl)
+        self.render_template('vivado_synth.tcl.j2', synth_tcl)
+
+        self._gen_scripts_name = (mk, proj_tcl, prog_tcl, run_tcl, synth_tcl)
+
+    def configure_main(self, non_lazy=False):
+        exists = os.path.exists
+        path_of = partial(os.path.join, self.work_root)
+        if self._gen_scripts_name:
+            all_exist = all(map(lambda x: exists(
+                path_of(x)), self._gen_scripts_name))
+        else:
+            all_exist = False
+        if not all_exist or non_lazy:
+            logger.debug('Non lazy configuration')
+            self.gen_scripts()
+        else:
+            logger.debug('Lazy configuration')
